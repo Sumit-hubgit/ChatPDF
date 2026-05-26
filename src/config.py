a9719669c5a7c878bba9d9af20d247c0d6543266
Sumit-hubgit <sumitsahu7017@gmail.com>
2026-05-26 21:28:05 +0530

    vectorstore

--- src/config.py
+++ src/config.py
@@ -1,10 +1,16 @@
+from qdrant_client.models import Distance
 from dotenv import load_dotenv
+import os
 load_dotenv()
 
 class Config:
-    embedding_model = "all-MiniLM-L6-v2"
-    chunking_model = "all-MiniLM-L6-v2"
-    breakpoint_threshold_type = "percentile"
-    breakpoint_threshold_amount=95
-    path = "data/pdf"
-
+    embedding_model: str = "all-MiniLM-L6-v2"
+    chunking_model: str = "all-MiniLM-L6-v2"
+    breakpoint_threshold_type: str = "percentile"
+    breakpoint_threshold_amount: int = 95
+    path: str = "data/pdf"
+    qdrant_api_key = os.getenv("QDRANT_API")
+    qdrant_url = os.getenv("CLUSTER_ENDPOINT")
+    collection_name: str ="ChatPDF"
+    vector_size = 384
+    vector_distance = Distance.COSINE
