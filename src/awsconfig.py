d447a26ae0d258f2f0effcb83c39ed42c50183f0
Sumit-hubgit <sumitsahu7017@gmail.com>
2026-06-06 23:07:04 +0530

    aws config

--- src/awsconfig.py
+++ src/awsconfig.py
@@ -1,1 +1,12 @@
-import boto
+import os
+import boto3
+s3_client = boto3.client('s3')
+s3_resource = boto3.resource('s3')
+response = s3_client.list_buckets()
+for bucket in response['Buckets']:
+    print(bucket)
+
+
+response = s3_client.list_objects_v2(Bucket='rag-pipeline1')
+objects = response.get('Content',[])
+print(objects)
